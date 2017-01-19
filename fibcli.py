659cab6f5456058be2af3371230d50379b6fab25
Cory Owens <Cory Owens>
2017-01-18 22:43:16 -0700

    Socket correctly working for arbitrarily large messages. Added run and test scripts.

--- fibcli.py
+++ fibcli.py
@@ -4,31 +4,29 @@ import socket
 import sys
 import struct
 
+import socketutils
+
 
 if len(sys.argv) != 2:
-    print("Usage: " + sys.argv[0] +" n");
-    print("Prints the nth Fibonacci number.");
+    print('Usage: ' + sys.argv[0] +' n');
+    print('Prints the nth Fibonacci number.');
     sys.exit(-1);
 
 try:
     n = int(sys.argv[1]);
-except ValueError: 
-    print("Invalid number: n must be an integer.");
+except ValueError:
+    print('Invalid number: n must be an integer.');
     sys.exit(-1);
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
 server_address = ('localhost', 10000);
 sock.connect(server_address);
 try:
-    request = n.to_bytes(16, byteorder='big');
-    sock.sendall(request);
-    amount_received = 0;
-    amount_expected = 1024;
-    while amount_received < amount_expected:
-        data = sock.recv(1024);
-        amount_received += len(data);
-        x = int.from_bytes(data, byteorder='big');
-        print(x);
+    message = n.to_bytes((n.bit_length() // 8) + 1, byteorder='little');
+    socketutils.send_msg(sock, message);
+    data = socketutils.recv_msg(sock);
+    x = int.from_bytes(data, byteorder='little');
+    print(x);
 finally:
     sock.close();
 
--- fibserver.py
+++ fibserver.py
@@ -1,46 +1,73 @@
 #!/usr/bin/python
 
+from functools import lru_cache
 import socket
 import sys
 import threading
 import time
 
 import fib
+import socketutils
+
+verbose = False;
+if len(sys.argv) > 1 and (sys.argv[1] == '-v' or sys.argv[1] == '-V'):
+    verbose = True;
+
+
+@lru_cache(maxsize=256)
+def cachedFib(n):
+    return fib.fib(n);
+
 
 class ServerThread (threading.Thread):
-    def run (self): 
+    def __init__(self, verbose):
+        super(ServerThread, self).__init__();
+        self.verbose = verbose;
+
+    def run(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
         server_address = ('localhost', 10000);
-        print('Starting up on %s port %s' % server_address);
+        if self.verbose:
+            print('Starting up on {0} port {1}'.format(server_address[0], server_address[1]));
         sock.bind(server_address);
         sock.listen(1);
-
-
         while True:
-            print('Waiting for a connection');
+            if self.verbose:
+                print('Waiting for a connection');
             connection, client_address = sock.accept();
             try:
-                print('Connection from ', client_address);
+                if self.verbose:
+                    print('Connection from ', client_address);
                 while True:
-                    data = connection.recv(16);
+                    if self.verbose:
+                        print('\tAttempting recv_msg');
+                    data = socketutils.recv_msg(connection);
                     if data:
-                        n = int.from_bytes(data, byteorder='big');
-                        print('Received "%i"' % n);
-                        x = fib.fib(n);
-                        print('Sending response back to client: %i' % x);
-                        connection.sendall(x.to_bytes(1024, byteorder='big'));
+                        n = int.from_bytes(data, byteorder='little');
+                        if self.verbose:
+                            print('\tn = ', n);
+                        x = cachedFib(n);
+                        if self.verbose:
+                            print('\tx = ', x);
+                        bit_length = x.bit_length();
+                        byte_length = bit_length // 8 + 1;
+                        response = x.to_bytes(byte_length, byteorder='little');
+                        if self.verbose:
+                            print('\tResponding...');
+                        socketutils.send_msg(connection, response);
                     else:
-                        print('No more data from ', client_address);
-                        break
-                    
+                        if self.verbose:
+                            print('\tNo more data from ', client_address);
+                        break;
             finally:
-                connection.close()
+                connection.close();
 
-s = ServerThread();
+
+s = ServerThread(verbose);
 s.daemon = True;
 s.start();
 try:
     while True:
         time.sleep(1);
-except KeyboardInterrupt: 
+except KeyboardInterrupt:
     sys.exit(0);
